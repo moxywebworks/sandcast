23efd9b63951e8ede5bab71522e6d7b3f2c2ee37
moxyinc <moxyinc@gmail.com>
2021-02-27 14:59:40 -0500

    Add files via upload

---
+++ bulma-test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My custom Bulma website</title>
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/library/mybulma/css/mystyles.css">
+<!--     <link rel="stylesheet" href="/Volumes/Macintosh%20HD/Users/moxy/Sites/moxywebworks.dv/wp-content/themes/moxy-master/library/mybulma/css/mystyles.css"> -->
+
+  </head>
+  <body>
+
+  </body>
+</html>
+
